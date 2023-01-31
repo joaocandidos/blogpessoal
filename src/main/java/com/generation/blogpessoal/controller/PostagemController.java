dc34306c4e28dae3537d5ff4e5ca08b6add38593
joaocandidos <120682757+joaocandidos@users.noreply.github.com>
2023-01-31 13:48:47 -0300

    add atividade blogpessoal 2

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -3,14 +3,15 @@ package com.generation.blogpessoal.controller;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/postagens")
@@ -20,9 +21,64 @@ public class PostagemController {
 
     @Autowired
     private PostagemRepository postagemRepository;
+
+
+
+    //metodo para acessar todas as postagens
     @GetMapping
     public ResponseEntity<List<Postagem>> getall(){
         return ResponseEntity.ok(postagemRepository.findAll());
     }
+
+    //metodo para fazer postagem
+    @PostMapping
+    public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(postagemRepository.save(postagem));
+    }
+
+    //metodo para acessar por id
+    @GetMapping("/{id}")
+    public ResponseEntity<Postagem> getById(@PathVariable Long id){
+        return postagemRepository.findById(id)
+                .map(resposta -> ResponseEntity.ok(resposta))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+
+    //metodo para acessar por titulo
+    @GetMapping("/titulo/{titulo}")
+    public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo){
+        return  ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+    }
+
+
+    //metodo paara atualizar as postagens
+    @PutMapping
+    public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem){
+        return postagemRepository.findById(postagem.getId())
+                .map( resposta -> ResponseEntity.status(HttpStatus.OK)
+                    .body(postagemRepository.save(postagem)))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+
+    }
+
+    //metodo para deletar postagens
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+       Optional<Postagem> postagem = postagemRepository.findById(id);
+
+       if(postagem.isEmpty())
+           throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+       postagemRepository.deleteById(id);
+   }
+
+
+
+
+
+
 
 }
--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -3,8 +3,20 @@ package com.generation.blogpessoal.repository;
 
 import com.generation.blogpessoal.model.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+
+    //query metodo = select * from tb_postagens where titulo like "%titulo%;
+    public  List <Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
+
 }
+
+
