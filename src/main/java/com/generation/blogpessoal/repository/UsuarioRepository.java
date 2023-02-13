8cb1545d3fed5618f2cda1d29b3c0cf8727f80c9
joaocandidos <120682757+joaocandidos@users.noreply.github.com>
2023-02-13 19:59:35 -0300

    adicionei arquivos de teste unitario

--- src/main/java/com/generation/blogpessoal/repository/UsuarioRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/UsuarioRepository.java
@@ -3,8 +3,10 @@ package com.generation.blogpessoal.repository;
 
 import com.generation.blogpessoal.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +14,7 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     //query metodo = SELECT * FROM tb_usuario WHERE usuario = "usuario";
     public Optional<Usuario> findByUsuario(String usuario);
+
+    public List<Usuario> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
+
 }
